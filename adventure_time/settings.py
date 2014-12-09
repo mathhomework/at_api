ec33f893921f97c956a9a181ac8f04c2d5dd4bc1
mathhomework <toxstalks@gmail.com>
2014-12-08 18:27:29 -0800

    pre aws

--- adventure_time/settings.py
+++ adventure_time/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'rest_framework',
     'at_api',
+    # 'rest_framework_swagger',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
