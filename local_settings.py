fbcd7427f09feb4c527b7f67030aad4c7b4c84d3
mathhomework <toxstalks@gmail.com>
2015-07-16 16:55:38 -0700

    removed some pycs

---
+++ local_settings.py
@@ -0,0 +1,8 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adventure_time',
+        'USER': 'postgres',
+        'PASSWORD': 'hotdog'
+    }
+}
